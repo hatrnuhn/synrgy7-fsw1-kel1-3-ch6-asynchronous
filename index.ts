f788dcbfe4e33b7c0d5e150f2e20cccbdf7ff51e
hatrnuhn <yusron.ibnunaim@gmail.com>
2024-05-27 20:55:54 +0700

    feat: uses environment variables

--- index.ts
+++ index.ts
@@ -2,22 +2,21 @@ import express, { Express, Request, Response } from "express";
 import knex from "knex";
 import { Model } from "objection";
 import router from "./src/routes";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Express = express();
-const port = 8000;
+const port = process.env.PORT ||  3000;
 
 const knexInstance = knex({
   client: "postgres",
-  connection: {
-    database: "db_tagihan",
-    user: "postgres",
-    password: "123456",
-    host: "127.0.0.1",
-    port: 5432,
-  },
+  connection: process.env.DATABASE_URL
 });
 
 Model.knex(knexInstance);
+
+console.log(port, ' ', process.env.DATABASE_URL);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- knexfile.ts
+++ knexfile.ts
@@ -5,13 +5,7 @@ import type { Knex } from "knex";
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: "postgres",
-    connection: {
-      database: "db_tagihan",
-      user: "postgres",
-      password: "123456",
-      host: "127.0.0.1",
-      port: 5432,
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       tableName: "knex_migrations",
       directory: "./src/database/migrations",
